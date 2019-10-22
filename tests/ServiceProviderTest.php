927a01ca35665758cd0368646d35788e16315dbb
Korobov Nikolay <korobov.n@avtocod.ru>
2019-10-22 17:39:00 +0500

    update illuminate up to 6.x

---
+++ tests/ServiceProviderTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace AvtoDev\HealthChecks\Tests;
+
+/**
+ * @covers \AvtoDev\HealthChecks\ServiceProvider<extended>
+ */
+class ServiceProviderTest extends AbstractTestCase
+{
+    /**
+     * @return void
+     */
+    public function test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
